2d022418eeb0907dc06295822259167ada17a7f4
Qiang Liu <cyruscyliu@gmail.com>
2019-12-10 19:05:32 +0800

    add update support list

--- build.py
+++ build.py
@@ -49,7 +49,71 @@ def build_hash_of_url_to_image_builder():
     return image_builder_table
 
 
-def build():
+def build(search=False):
+    # 6. write all to support_list.csv
+    if search:
+        support_list = {}
+        for one_building_dir in os.listdir('share'):
+            full_path = os.path.join('share', one_building_dir)
+            if os.path.isfile(full_path):
+                continue
+            openwrtver, _, hash_of_image_builder = one_building_dir.partition('-')
+            if not len(hash_of_image_builder):
+                continue
+            # find vmlinux.elf-debug-info
+            vmlinux_debug_results = os.popen('cd {} && find -name vmlinux.elf-debug-info'.format(full_path)).readlines()
+            set_vmlinux_debug = set([os.path.dirname(path) for path in vmlinux_debug_results])
+            # find vmlinux
+            vmlinux_results = os.popen('cd {} && find -name vmlinux'.format(full_path)).readlines()
+            set_vmlinux = set([os.path.dirname(os.path.dirname(path)) for path in vmlinux_results])
+            target_dirs = set_vmlinux_debug & set_vmlinux
+            if not len(target_dirs):
+                continue
+            # find .config
+            dot_config_results = os.popen('cd {} && find -name .config'.format(full_path)).readlines()
+            set_config = set([os.path.dirname(os.path.dirname(path)) for path in dot_config_results])
+            target_dirs = target_dirs & set_config
+            if not len(target_dirs):
+                continue
+            # fill in support list
+            support_list[hash_of_image_builder] = {}
+            target_dir = list(target_dirs)[0]
+            # vmlinux.elf-debug-info
+            for path in vmlinux_debug_results:
+                if os.path.dirname(path) == target_dir:
+                    support_list[hash_of_image_builder]['path_to_vmlinux_debug_info'] = os.path.join(full_path, path.strip())
+            # source code
+            for path in vmlinux_results:
+                if os.path.dirname(os.path.dirname(path)) == target_dir and os.path.basename(os.path.dirname(path)).find('linux') != -1:
+                    support_list[hash_of_image_builder]['path_to_source_code'] = os.path.join(full_path, os.path.dirname(path.strip()))
+                    support_list[hash_of_image_builder]['path_to_vmlinux'] = os.path.join(full_path, path.strip())
+                    support_list[hash_of_image_builder]['path_to_dot_config'] = os.path.join(full_path, os.path.join(os.path.dirname(path.strip()), '.config'))
+
+        old_support_list = {}
+        if os.path.exists('support_list.csv'):
+            with open('support_list.csv', 'r') as f:
+                for line in f:
+                    hash_of_image_builder, vmlinux_debug_info, source_code, vmlinux, dot_config = line.strip().split(',')
+                    if hash_of_image_builder in support_list:
+                        continue
+                    old_support_list[hash_of_image_builder] = {
+                            'path_to_vmlinux_debug_info': vmlinux_debug_info,
+                            'path_to_source_code': source_code,
+                            'path_to_vmlinux': vmlinux,
+                            'path_to_dot_config': dot_config
+                    }
+
+        with open('support_list.csv', 'w') as f:
+            for h, v in old_support_list.items():
+                f.write(','.join([h, v['path_to_vmlinux_debug_info'], v['path_to_source_code'], v['path_to_vmlinux'], v['path_to_dot_config']]))
+                f.write('\n')
+            for h, v in support_list.items():
+                f.write(','.join([h, v['path_to_vmlinux_debug_info'], v['path_to_source_code'], v['path_to_vmlinux'], v['path_to_dot_config']]))
+                f.write('\n')
+
+        exit(0)
+
+
     # we found a .config always in an ImageBuilder
     if not os.path.exists('image_builder.csv'):
         image_builder_table = build_hash_of_url_to_image_builder()
@@ -63,8 +127,6 @@ def build():
             for line in f:
                 items = line.strip().split(',')
                 image_builder_table[items[0]] = {'url':items[1], 'support':items[2:]}
-
-    #print(image_builder_table)
 
     # in this loop, prepare the building directory for every firmware
     for k, v in image_builder_table.items():
@@ -93,13 +155,13 @@ def build():
         # 4. copy other things (patches to download.pl, makefiles, etc...)
         os.system('cp share/%s/* share/%s' % (openwrtver, one_building_dir))
 
-    # 5. build the machine in the specified docker (maybe manually?)
-
-    # 6. write all to support list
-    # we need to locate the path:
-    # the easiest way is to search the vmlinux.elf first,
-    # then locate the linux source dir based on that
-
+        # 5. build the machine in the specified docker (manually)
 
 if __name__ == '__main__':
-    build()
+    if len(sys.argv) == 2 and sys.argv[1] == '-s':
+        build(search=True)
+    elif len(sys.argv) == 1:
+        build()
+    else:
+        print('usage {} [-s]'.format(sys.argv[0]))
+
