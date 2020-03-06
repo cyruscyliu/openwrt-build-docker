a8b7ca5934bbf1a25c13507057f05678d356bd3e
Zhang Cen <blbllhy@gmail.com>
2020-03-06 13:02:08 +0800

    make new build feature support 10.03

--- compile_package/17.01/compile.sh
+++ compile_package/17.01/compile.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+set -x
+
 script_dir=`dirname $0`
 
-yml_path=${script_dir}/docker-compose.yml
+yml_temp=${script_dir}/docker-compose.tmp
 build_dir=$1
 predownload_dir=`realpath ${script_dir}/../../pre_download`
 
 cd ${build_dir}
-COMPOSE_HTTP_TIMEOUT=120 docker-compose -f ${yml_path} up -e BUILD_DIR=${build_dir} -e CACHE_DIR=${predownload_dir}
+sed "s!BUILD_DIR!${build_dir}!g" ${yml_temp} | sed "s!CACHE_DIR!${predownload_dir}!g" > docker-compose.yml
+COMPOSE_HTTP_TIMEOUT=120 docker-compose -f docker-compose.yml run firmware-build
