8bf81da6d7874df6fae480ecc665df0d01d4ec09
Zhang Cen <blbllhy@gmail.com>
2020-03-07 13:04:58 +0800

    update compile & build script for better automatic parallel building with autobots

--- compile_package/10.03/build.sh
+++ compile_package/10.03/build.sh
@@ -1,1 +1,4 @@
-docker build . -t openwrt-10.03:latest
+if [[ "$(docker images -q openwrt-10.03:latest 2> /dev/null)" == "" ]]; then
+  # do something
+  docker build . -t openwrt-10.03:latest
+fi
--- compile_package/15.05/build.sh
+++ compile_package/15.05/build.sh
@@ -1,1 +1,4 @@
-docker build . -t openwrt-15.05:latest
+if [[ "$(docker images -q openwrt-15.05:latest 2> /dev/null)" == "" ]]; then
+  # do something
+  docker build . -t openwrt-15.05:latest
+fi
